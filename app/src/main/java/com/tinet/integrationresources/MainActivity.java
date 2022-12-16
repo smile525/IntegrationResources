7aa91f00724b2487db3ed585a9d23eeabea1dade
smile525 <zp045217@163.com>
2022-12-16 15:27:24 +0800

    第一次提交

---
+++ app/src/main/java/com/tinet/integrationresources/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tinet.integrationresources;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
